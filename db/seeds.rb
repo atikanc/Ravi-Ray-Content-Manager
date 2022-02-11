ec2504d76ade9fa706a0f425481138e2b21f6179
atikanc <atikanchokpapone@gmail.com>
2022-02-11 16:48:09 -0600

    Added Input Requirements and Cleaned up UI
    Added requirements to certain fields. Changed the IDs to the name of the item being referenced.

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,20 +4,23 @@ Award.create!([
   {AwardName: "Gold Award", AwardLink: "", AwardYear: "2022-02-11"}
 ])
 Awarded.create!([
-  {AwardedDate: "2022-02-11", AwardID: 1, ProjectID: 1, awards_id: nil, projects_id: nil}
+  {AwardedDate: "2022-02-11", AwardID: 1, ProjectID: 1, awards_id: 1, projects_id: 1},
+  {AwardedDate: "2022-02-11", AwardID: 3, ProjectID: 1, awards_id: 1, projects_id: 1}
 ])
 Contribution.create!([
   {ContributionType: "Mastered"},
   {ContributionType: "Composed"},
   {ContributionType: "Mix Assistant"},
   {ContributionType: "Mixed"},
-  {ContributionType: "Produced"}
+  {ContributionType: "Produced"},
 ])
 DisplayLine.create!([
-  {ComponentContributed: "Episode 4", ComponentStartDate: "2022-02-11", ComponentEndDate: "2022-02-11", ContribType: 1}
+  {ComponentContributed: "Episode 4", ComponentStartDate: "2022-02-11", ComponentEndDate: "2022-02-11", ContribProject: 1, ContribType: 1},
+  {ComponentContributed: "Episode 7", ComponentStartDate: "2022-02-11", ComponentEndDate: "2022-02-11", ContribProject: 1, ContribType: 1}
 ])
 Project.create!([
-  {ProjectName: "Macey", ProjectLink: "", ProjectOwner: "Max", ProjectStart: "2022-02-11", ProjectEnd: "2022-02-11", ProjectDescription: "", TypeID: 2}
+  {ProjectName: "Fantastic", ProjectLink: "", ProjectOwner: "Billy", ProjectStart: "2022-02-11", ProjectEnd: "2022-02-11", ProjectDescription: "", TypeID: 2},
+  {ProjectName: "Spectacular", ProjectLink: "", ProjectOwner: "Bobby", ProjectStart: "2022-02-11", ProjectEnd: "2022-02-11", ProjectDescription: "", TypeID: 1}
 ])
 Type.create!([
   {TypeName: "Music"},

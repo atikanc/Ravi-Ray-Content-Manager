19a9b511ac8471cd75e325c9da6f6b9ccc53e98e
atikanc <atikanchokpapone@gmail.com>
2022-02-11 15:14:24 -0600

    Fixed Models and Project Details
    Fixed a tad of everything.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Award.create!([
+  {AwardName: "Best Business/Finance Podcast Award", AwardLink: "", AwardYear: "2022-02-11"},
+  {AwardName: "Best Business Podcast Award", AwardLink: "", AwardYear: "2022-02-11"},
+  {AwardName: "Gold Award", AwardLink: "", AwardYear: "2022-02-11"}
+])
+Awarded.create!([
+  {AwardedDate: "2022-02-11", AwardID: 1, ProjectID: 1, awards_id: nil, projects_id: nil}
+])
+Contribution.create!([
+  {ContributionType: "Mastered"},
+  {ContributionType: "Composed"},
+  {ContributionType: "Mix Assistant"},
+  {ContributionType: "Mixed"},
+  {ContributionType: "Produced"}
+])
+DisplayLine.create!([
+  {ComponentContributed: "Episode 4", ComponentStartDate: "2022-02-11", ComponentEndDate: "2022-02-11", ContribType: 1}
+])
+Project.create!([
+  {ProjectName: "Macey", ProjectLink: "", ProjectOwner: "Max", ProjectStart: "2022-02-11", ProjectEnd: "2022-02-11", ProjectDescription: "", TypeID: 2}
+])
+Type.create!([
+  {TypeName: "Music"},
+  {TypeName: "Podcast"},
+  {TypeName: "Audiobook"},
+  {TypeName: "Film Scoring"},
+  {TypeName: "Movie"},
+  {TypeName: "Other"}
+])

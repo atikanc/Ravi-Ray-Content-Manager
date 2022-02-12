84df3f8bf5060e5ee54b209da31968baa23231c2
cjnewman820 <95238267+cjnewman820@users.noreply.github.com>
2022-02-12 16:45:42 -0600

    Unit and integration test for "Awarded"

--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -15,4 +15,29 @@ RSpec.describe Type, type: :model do
       expect(subject).not_to be_valid
     end
   end
+
+RSpec.describe Awarded, type: :model do
+  subject do
+    described_class.new(AwardedDate: '2022-02-14', AwardID: 1, ProjectID: 1)
+  end
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without a date' do
+    subject.AwardedDate = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without a award id' do
+    subject.AwardID = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without a project id' do
+    subject.ProjectID = nil
+    expect(subject).not_to be_valid
+  end
+end
   

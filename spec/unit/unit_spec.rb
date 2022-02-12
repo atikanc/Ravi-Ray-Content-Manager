95281f3a0f2f70da81581389df13c5d34e3426a5
AlexanderHallCS <hallale21@yahoo.com>
2022-02-12 16:49:09 -0600

    Add unit/integration tests for Awards

--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -16,3 +16,33 @@ RSpec.describe Type, type: :model do
     end
   end
   
+RSpec.describe Award, type: :model do
+  subject do
+    described_class.new(AwardName: 'Test Award', AwardLink: 'award.com', AwardYear: '2022-02-14')
+  end
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without a name' do
+    subject.AwardName = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without a year' do
+    subject.AwardYear = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is valid with a name' do
+    subject.AwardName = 'Test Award'
+    expect(subject).to be_valid
+  end
+
+  it 'is valid with a year' do
+    subject.AwardYear = '2022-02-14'
+    expect(subject).to be_valid
+  end
+
+end

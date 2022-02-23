4b8740afc23dac7dd8fadb6b452e6362174e922e
atikanc <atikanchokpapone@gmail.com>
2022-02-23 14:57:04 -0600

    Link Sanitization
    Added some link sanitization to the project creation page. This fixes the problem of RoR trying to use the user-provided links that do not contain "http" as a route (appends link to current url).

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,14 @@ class Project < ApplicationRecord
   has_one_attached :ProjectCover
   validates :TypeID, presence: true
   has_one :Contribution, through: :DisplayLine
+
+  # From https://stackoverflow.com/questions/36038646/string-interpolation-to-external-link-without-http
+  # For Input Sanitization
+  def ProjectLink=(url)
+    if url.present?
+      url = url.match(/https?:\/\//) ? url : "http://#{url}"
+      write_attribute(:ProjectLink, url)
+    end
+  end
+  
 end

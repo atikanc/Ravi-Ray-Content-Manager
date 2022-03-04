25816619d01b3ce75a516ea0a7ede9c8d662f84c
Bao-Vo-Creator <vodinhtbao@gmail.com>
2022-03-03 20:02:57 -0600

    created the unit and integration tests for filtering by type

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -2,13 +2,13 @@
 require 'rails_helper'
 
 RSpec.describe 'Creating a type', type: :feature do
-    scenario 'valid inputs' do
-      visit new_type_path
-      fill_in 'type_TypeName', with: 'Music'
-      click_on 'Create Type'
-      visit types_path
-      expect(page).to have_content('Music')
-    end
+  scenario 'valid inputs' do
+    visit new_type_path
+    fill_in 'type_TypeName', with: 'Music'
+    click_on 'Create Type'
+    visit types_path
+    expect(page).to have_content('Music')
+  end
 end
 
 RSpec.describe('Creating an award', type: :feature) do
@@ -66,96 +66,150 @@ RSpec.describe 'Creating an awarded', type: :feature do
     expect(page).to have_content('2022-02-14')
     expect(page).to have_content('Coolest Song')
     expect(page).to have_content('Cool Song')
-    end
-  end  
+  end
+end  
+
 RSpec.describe 'Creating a project', type: :feature do
   scenario 'valid inputs' do
-      # make music type
-      visit new_type_path
-      fill_in 'type_TypeName', with: 'Music'
-      click_on 'Create Type'
-      visit types_path
-      expect(page).to have_content('Music')
+    # make music type
+    visit new_type_path
+    fill_in 'type_TypeName', with: 'Music'
+    click_on 'Create Type'
+    visit types_path
+    expect(page).to have_content('Music')
 
-      visit new_project_path
-      fill_in 'project_ProjectName', with: 'projectname1'
-      fill_in 'project_ProjectLink', with: 'projectlink1'
-      fill_in 'project_ProjectOwner', with: 'projectowner1'
-      fill_in 'project_ProjectDescription', with: 'projectdescription1'
-      select 2017, :from => 'project[ProjectStart(1i)]'
-      select 'January', :from => 'project[ProjectStart(2i)]'
-      select 30, :from => 'project[ProjectStart(3i)]'
-      select 2021, :from => 'project[ProjectEnd(1i)]'
-      select 'January', :from => 'project[ProjectEnd(2i)]'
-      select 29, :from => 'project[ProjectEnd(3i)]'
-      find('#project_TypeID').find(:xpath, 'option[2]').select_option
-      click_on 'Create Project'
-      visit projects_path
-      expect(page).to have_content('2021')
-      expect(page).to have_content('29')
-      expect(page).to have_content('projectname1')
-      expect(page).to have_link(href: 'http://projectlink1')
-      expect(page).to have_content('projectowner1')
-      expect(page).to have_content('Music')
-    end
+    visit new_project_path
+    fill_in 'project_ProjectName', with: 'projectname1'
+    fill_in 'project_ProjectLink', with: 'projectlink1'
+    fill_in 'project_ProjectOwner', with: 'projectowner1'
+    fill_in 'project_ProjectDescription', with: 'projectdescription1'
+    select 2017, :from => 'project[ProjectStart(1i)]'
+    select 'January', :from => 'project[ProjectStart(2i)]'
+    select 30, :from => 'project[ProjectStart(3i)]'
+    select 2021, :from => 'project[ProjectEnd(1i)]'
+    select 'January', :from => 'project[ProjectEnd(2i)]'
+    select 29, :from => 'project[ProjectEnd(3i)]'
+    find('#project_TypeID').find(:xpath, 'option[2]').select_option
+    click_on 'Create Project'
+    visit projects_path
+    expect(page).to have_content('2021')
+    expect(page).to have_content('29')
+    expect(page).to have_content('projectname1')
+    expect(page).to have_link(href: 'http://projectlink1')
+    expect(page).to have_content('projectowner1')
+    expect(page).to have_content('Music')
   end
+
+  scenario 'filtering the correct projects by type' do
+    #Make music type
+    visit new_type_path
+    fill_in 'type_TypeName', with: 'Music'
+    click_on 'Create Type'
+    
+    #Make podcast type
+    visit new_type_path
+    fill_in 'type_TypeName', with: 'Podcast'
+    click_on 'Create Type'
+
+    #Make the music project
+    visit new_project_path
+    fill_in 'project_ProjectName', with: 'projectname1'
+    fill_in 'project_ProjectLink', with: 'projectlink1'
+    fill_in 'project_ProjectOwner', with: 'projectowner1'
+    fill_in 'project_ProjectDescription', with: 'projectdescription1'
+    select 2017, :from => 'project[ProjectStart(1i)]'
+    select 'January', :from => 'project[ProjectStart(2i)]'
+    select 30, :from => 'project[ProjectStart(3i)]'
+    select 2021, :from => 'project[ProjectEnd(1i)]'
+    select 'January', :from => 'project[ProjectEnd(2i)]'
+    select 29, :from => 'project[ProjectEnd(3i)]'
+    find('#project_TypeID').find(:xpath, 'option[2]').select_option
+    click_on 'Create Project'
+
+    #Make the podcast project
+    visit new_project_path
+    fill_in 'project_ProjectName', with: 'projectname2'
+    fill_in 'project_ProjectLink', with: 'projectlink2'
+    fill_in 'project_ProjectOwner', with: 'projectowner2'
+    fill_in 'project_ProjectDescription', with: 'projectdescription2'
+    select 2017, :from => 'project[ProjectStart(1i)]'
+    select 'January', :from => 'project[ProjectStart(2i)]'
+    select 30, :from => 'project[ProjectStart(3i)]'
+    select 2022, :from => 'project[ProjectEnd(1i)]'
+    select 'January', :from => 'project[ProjectEnd(2i)]'
+    select 29, :from => 'project[ProjectEnd(3i)]'
+    find('#project_TypeID').find(:xpath, 'option[3]').select_option
+    click_on 'Create Project'
+
+    #what the hell is going on
+    visit projects_path
+    fill_in 'search', with: "\"Podcast\"" 
+    click_on 'Search'
+    expect(page).to have_content('2022')
+    expect(page).to have_content('29')
+    expect(page).to have_content('projectname2')
+    expect(page).to have_link(href: 'http://projectlink1')
+    expect(page).to have_content('projectowner2')
+    expect(page).to have_content("Podcast")
+  end
+end
 
 RSpec.describe 'Creating a display line', type: :feature do
-    scenario 'valid inputs' do
-      # make music type
-      visit new_type_path
-      fill_in 'type_TypeName', with: 'Music'
-      click_on 'Create Type'
-      visit types_path
-      expect(page).to have_content('Music')
+  scenario 'valid inputs' do
+    # make music type
+    visit new_type_path
+    fill_in 'type_TypeName', with: 'Music'
+    click_on 'Create Type'
+    visit types_path
+    expect(page).to have_content('Music')
 
-      # make project
-      visit new_project_path
-      fill_in 'project_ProjectName', with: 'projectname1'
-      fill_in 'project_ProjectLink', with: 'projectlink1'
-      fill_in 'project_ProjectOwner', with: 'projectowner1'
-      fill_in 'project_ProjectDescription', with: 'projectdescription1'
-      select 2017, :from => 'project[ProjectStart(1i)]'
-      select 'January', :from => 'project[ProjectStart(2i)]'
-      select 30, :from => 'project[ProjectStart(3i)]'
-      select 2021, :from => 'project[ProjectEnd(1i)]'
-      select 'January', :from => 'project[ProjectEnd(2i)]'
-      select 29, :from => 'project[ProjectEnd(3i)]'
-      find('#project_TypeID').find(:xpath, 'option[2]').select_option
-      click_on 'Create Project'
+    # make project
+    visit new_project_path
+    fill_in 'project_ProjectName', with: 'projectname1'
+    fill_in 'project_ProjectLink', with: 'projectlink1'
+    fill_in 'project_ProjectOwner', with: 'projectowner1'
+    fill_in 'project_ProjectDescription', with: 'projectdescription1'
+    select 2017, :from => 'project[ProjectStart(1i)]'
+    select 'January', :from => 'project[ProjectStart(2i)]'
+    select 30, :from => 'project[ProjectStart(3i)]'
+    select 2021, :from => 'project[ProjectEnd(1i)]'
+    select 'January', :from => 'project[ProjectEnd(2i)]'
+    select 29, :from => 'project[ProjectEnd(3i)]'
+    find('#project_TypeID').find(:xpath, 'option[2]').select_option
+    click_on 'Create Project'
 
-      # make contribution type
-      visit new_contribution_path
-      fill_in 'contribution_ContributionType', with: 'contributiontype'
-      click_on 'Create Contribution'
+    # make contribution type
+    visit new_contribution_path
+    fill_in 'contribution_ContributionType', with: 'contributiontype'
+    click_on 'Create Contribution'
 
-      visit new_display_line_path
-      fill_in 'display_line_ComponentContributed', with: 'something1'
-      select 2017, :from => 'display_line[ComponentStartDate(1i)]'
-      select 'January', :from => 'display_line[ComponentStartDate(2i)]'
-      select 30, :from => 'display_line[ComponentStartDate(3i)]'
-      select 2021, :from => 'display_line[ComponentEndDate(1i)]'
-      select 'January', :from => 'display_line[ComponentEndDate(2i)]'
-      select 29, :from => 'display_line[ComponentEndDate(3i)]'
-      find('#display_line_Project_id').find(:xpath, 'option[2]').select_option
-      find('#display_line_Contribution_id').find(:xpath, 'option[2]').select_option
-      click_on 'Create Display line'
-      visit display_lines_path
-      # expect(page).to have_content('contributiontype')
-      expect(page).to have_content('2017-01-30')
-      expect(page).to have_content('2021-01-29')
-      expect(page).to have_content('something1')
-      # expect(page).to have_content('projectname1')
-    end
+    visit new_display_line_path
+    fill_in 'display_line_ComponentContributed', with: 'something1'
+    select 2017, :from => 'display_line[ComponentStartDate(1i)]'
+    select 'January', :from => 'display_line[ComponentStartDate(2i)]'
+    select 30, :from => 'display_line[ComponentStartDate(3i)]'
+    select 2021, :from => 'display_line[ComponentEndDate(1i)]'
+    select 'January', :from => 'display_line[ComponentEndDate(2i)]'
+    select 29, :from => 'display_line[ComponentEndDate(3i)]'
+    find('#display_line_Project_id').find(:xpath, 'option[2]').select_option
+    find('#display_line_Contribution_id').find(:xpath, 'option[2]').select_option
+    click_on 'Create Display line'
+    visit display_lines_path
+    # expect(page).to have_content('contributiontype')
+    expect(page).to have_content('2017-01-30')
+    expect(page).to have_content('2021-01-29')
+    expect(page).to have_content('something1')
+    # expect(page).to have_content('projectname1')
   end
+end
 
-  RSpec.describe 'Creating a contribution type', type: :feature do
-    scenario 'valid inputs' do
-      visit new_contribution_path
-      fill_in 'contribution_ContributionType', with: 'Bao Type'
-      click_on 'Create Contribution'
-      visit contributions_path
-      expect(page).to have_content('Bao Type')
-    end
+RSpec.describe 'Creating a contribution type', type: :feature do
+  scenario 'valid inputs' do
+    visit new_contribution_path
+    fill_in 'contribution_ContributionType', with: 'Bao Type'
+    click_on 'Create Contribution'
+    visit contributions_path
+    expect(page).to have_content('Bao Type')
   end
+end
   

020c22e1e611df157d55460662d49c5eaebef548
AlexanderHallCS <hallale21@yahoo.com>
2022-03-05 17:32:51 -0600

    OAuth test 4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,7 +118,7 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :google_oauth2, ENV["CLIENT_ID"], ENV["CLIENT_SECRET"]
+    provider :google_oauth2, ENV["CLIENT_ID"], ENV["CLIENT_SECRET"], provider_ignores_state: true
   end
   # GOOGLE_OAUTH_CLIENT_ID = ENV['CLIENT_ID']
   # GOOGLE_OAUTH_CLIENT_SECRET = ENV['CLIENT_SECRET']

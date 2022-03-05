60795af6840388e749c38a11a68f68eb856c82d8
AlexanderHallCS <hallale21@yahoo.com>
2022-03-05 05:59:40 -0600

    Add OAuth with some non-passing tests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+   # TODO: Uncomment this when done testing or in production, comment when testing
+    before_action :authenticate_admin!
+   # TODO: Add "skip_before_filter :authenticate_user!" to the top of the controller file 
+   # of whichever new controller will only display projects(user side)
 end

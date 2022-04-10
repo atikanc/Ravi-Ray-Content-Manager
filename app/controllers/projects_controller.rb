e1d04f1770ede73d4920cab2faca53a0d99cb9ee
AlexanderHallCS <hallale21@yahoo.com>
2022-04-10 00:07:34 -0500

    Make projects/show page require login

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,6 @@
 class ProjectsController < ApplicationController
   before_action :set_project, only: %i[show edit update destroy]
-  skip_before_action :authenticate_admin!, only: [:public, :show]
+  skip_before_action :authenticate_admin!, only: [:public]
 
   # GET /projects or /projects.json
   def index

3864ac82cb1fedc76771d8732f0e058ea7a07a61
AlexanderHallCS <hallale21@yahoo.com>
2022-04-21 19:38:31 -0500

    Add environment variables for emails, remove bullet points, add sorting on admin page

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -5,7 +5,7 @@ class Admin < ApplicationRecord
   validates :email, presence: true
 
   def self.from_google(email:, full_name:, uid:, avatar_url:)
-    return nil unless (email == "roboburner13@gmail.com" || email == "ravi@ravirayaudio.com")
+    return nil unless (email == ENV['ADMIN_EMAIL_ONE'] || email == ENV['ADMIN_EMAIL_TWO'])
     create_with(uid: uid, full_name: full_name, avatar_url: avatar_url).find_or_create_by!(email: email)
   end
 end
